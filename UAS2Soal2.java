4977de8928d724491c4eef7029ad4e57aca81050
salsabillaannasyhar <81260403+salsabillaannasyhar@users.noreply.github.com>
2021-06-29 09:45:56 +0700

    Add files via upload

---
+++ UAS2Soal2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas2soal.pkg2;
+
+/**
+ *
+ * @author Salsabilla Annasyhar
+ */
+public class UAS2Soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
